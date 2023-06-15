a1bc18096419fd28e630d46f5e6448846b6a5f06
brandonw5442 <113524441+brandonw5442@users.noreply.github.com>
2023-06-15 12:58:41 -0400

    Submitted: 6-15-2023
    P3LAB_BrandonWilliam

---
+++ P3LAB_BrandonWilliam.py
@@ -0,0 +1,22 @@
+is_leap_year = False
+   
+input_year = int(input())
+
+if (input_year % 4) == 0:
+    is_leap_year = True
+else:
+    is_leap_year = False
+    
+    
+if (input_year % 100) == 0:
+    is_leap_year = False
+if (input_year % 100) == 0 and (input_year % 400) == 0:
+    is_leap_year = True
+
+
+if is_leap_year == 1:
+    print(input_year, '- leap year')
+else:
+    print(input_year, '- not a leap year')
+
+    
