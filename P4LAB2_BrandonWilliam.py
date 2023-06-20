ed3130b23c4e5d291510695b70b1def102b8c0ff
brandonw5442 <113524441+brandonw5442@users.noreply.github.com>
2023-06-20 16:10:40 -0400

    Submitted 6-20-2023 @4:10
    P4LAB2_BrandonWilliam

---
+++ P4LAB2_BrandonWilliam.py
@@ -0,0 +1,18 @@
+num1 = int(input())
+num2 = int(input())
+
+
+
+if num1 > num2:
+    print("Second integer can't be less than the first.")  
+else:
+    while num1 <= num2:
+        print(num1, end = ' ')
+        num1 += 5
+        if num1 > num2:
+            print()
+
+    
+
+
+    
